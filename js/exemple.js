8f490cd1759876bdc47e2df974c3f5369e4cffa0
daglumbussigmaalphamale <francois.s.favrat@gmail.com>
2025-11-11 09:00:09 +0100

    update

---
+++ js/exemple.js
@@ -0,0 +1,27 @@
+var message = 'Bienvenue';
+
+
+
+
+//console.log('message');
+
+//function ma_fonction() {
+//    console.log('ma fonciton');
+//}
+
+//ma_fonction();
+
+console.log(document.querySelector('.important').textContent)
+
+//for (var i = 0; i < 10; i++) {
+//    console.log(i);
+//}
+
+function ajouter_perso() {
+    var nom_perso = 'ehgros';
+
+    var nouveau_li = document.createElement('li');
+    var nouveau_text = document.createTextNode(text_perso);
+
+    console.log('perso');
+}
